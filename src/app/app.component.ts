b0a3019b9ee554c8b204361cb8f489a51dad82d0
Charles Bon Chua <chuacharlesbon@gmail.com>
2025-09-13 15:33:36 +0800

    Test: SSR Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +10,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-19-app';
+
+  meta = inject(Meta);
+  appTitle = inject(Title);
+
+  ngOnInit(): void {
+    this.appTitle.setTitle('Main - My Angular App');
+    this.meta.updateTag({ name: 'description', content: 'Main Page - Learn more about us on this page.' });
+    this.meta.updateTag({ property: 'og:title', content: 'Main - My Angular App' });
+  }
+
 }
