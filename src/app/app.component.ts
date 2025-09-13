dd78cd31badb83a0705f23121d2f1963acf840e4
Charles Bon Chua <chuacharlesbon@gmail.com>
2025-09-13 17:13:42 +0800

    Added: Translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,10 +15,23 @@ export class AppComponent {
   meta = inject(Meta);
   appTitle = inject(Title);
 
+  // Language
+  private translate = inject(TranslateService);
+  currentLang = this.translate.getFallbackLang() || 'en';
+
+  changeLanguage() {
+    this.translate.use(this.currentLang === "ja" ? "en" : "ja"); // dynamically switch
+  }
+
   ngOnInit(): void {
+    this.translate.use('ja');
     this.appTitle.setTitle('Main - My Angular App');
     this.meta.updateTag({ name: 'description', content: 'Main Page - Learn more about us on this page.' });
     this.meta.updateTag({ property: 'og:title', content: 'Main - My Angular App' });
-  }
 
+    // listen for changes
+    this.translate.onLangChange.subscribe(event => {
+      this.currentLang = event.lang;
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { CustomTranslateLoader } from './utils/translate-loader';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withFetch()
+    ),
+    provideTranslateService({
+      fallbackLang: 'ja',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => new CustomTranslateLoader(http),
+        deps: [HttpClient],
+      },
+    }),
+  ]
 };
