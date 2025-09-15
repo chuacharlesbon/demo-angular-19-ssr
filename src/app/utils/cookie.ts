37d963c0204de8e89af8da51492c3c33daf51507
Charles Bon Chua <chuacharlesbon@gmail.com>
2025-09-15 18:05:52 +0800

    Added: Brand Logo

--- src/app/utils/cookie.ts
+++ src/app/utils/cookie.ts
@@ -1,4 +1,14 @@
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
 export function getCookie(name: string): string | null {
+  const platformId = inject(PLATFORM_ID);
+
+  // Only run in browser (document is undefined on the server)
+  if (isPlatformBrowser(platformId)) {
     const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
     return match ? decodeURIComponent(match[2]) : null;
+  }
+
+  return null; // SSR always returns null
 }
