fe36dab4ab0a48e43618ed635a5c84abe2efc5e0
Charles Bon Chua <chuacharlesbon@gmail.com>
2025-09-15 17:48:41 +0800

    Added: Core Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslateModule],
+  imports: [RouterOutlet, TranslateModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,109 @@
+import { Component, inject, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { NgbDropdownModule, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { filter, Subscription } from 'rxjs';
+import { routeNames } from '../../app.routes';
+import { User } from '../../core/interfaces/user';
+import { ProfileStore } from '../../stores/profileStore';
+import { getCookie } from '../../utils/cookie';
+import { Status } from '../../utils/enums';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, NgbDropdownModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  // Language
+  private translate = inject(TranslateService);
+  changeLanguage(lang: string) {
+    this.translate.use(lang); // dynamically switch
+  }
+
+  get currentLanguage(): string {
+    return this.translate.currentLang;
+  }
+
+  // Track Navigation
+  offcanvasService = inject(NgbOffcanvas);
+  router = inject(Router);
+  isLoginPage = false;
+  nonUserRoutes = [
+    "/",
+    "/logout",
+    "/forgot-password",
+  ];
+  userLink = "/";
+  productLink = "/";
+
+  private trackNavigation() {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event: any) => {
+        const navEndEvent = event as NavigationEnd; // ✅ Explicit type assertion
+        if (this.nonUserRoutes.includes(navEndEvent.urlAfterRedirects)) {
+          this.isLoginPage = true;
+        } else {
+          this.isLoginPage = false;
+        }
+      });
+  }
+
+  // Drawer Component
+  @ViewChild('drawer') drawer: any;
+
+  openDrawer() {
+    this.offcanvasService.open(this.drawer, { position: 'start' }); // 'start' = left side
+  }
+
+  navigateAndClose(offcanvas: any, path: string) {
+    this.router.navigate([path]);
+    offcanvas.dismiss();
+  }
+
+  // Dropdown
+  onAccount() {
+    //
+  }
+
+  onLogout() {
+    const emptyUser: User = {};
+    this.profileStore.setProfile(emptyUser);
+    this.router.navigate([routeNames.login.path]);
+  }
+
+  // Profile
+  profileStore = inject(ProfileStore);
+  profile = this.profileStore.profile$;
+  status = this.profileStore.status$;
+  profileSub = Subscription.EMPTY;
+  statusSub = Subscription.EMPTY;
+  profileData: User = {};
+
+  ngOnInit() {
+    this.translate.use('ja');
+    this.trackNavigation();
+    this.profileSub = this.profile.subscribe((data) => {
+      this.profileData = data;
+    });
+    this.statusSub = this.status.subscribe((data) => {
+      const hasToken = getCookie('user-session');
+      if (data === Status.Initial && hasToken) {
+        // this.profileStore.getProfile();
+      }
+    });
+    this.translate.onLangChange.subscribe((event) => {
+      console.log('Language changed to:', event.lang);
+    });
+  }
+
+  ngOnDestroy() {
+    this.profileSub.unsubscribe(); // Prevent memory leaks
+    this.statusSub.unsubscribe(); // Prevent memory leaks
+  }
+}
